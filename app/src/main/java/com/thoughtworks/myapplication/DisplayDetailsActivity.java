2c4843f45a4ce3c452a1444773179b14754f7f58
crystalynliu <crystalynliu@163.com>
2015-12-25 23:30:20 +0800

    add ImageView

--- app/src/main/java/com/thoughtworks/myapplication/DisplayDetailsActivity.java
+++ app/src/main/java/com/thoughtworks/myapplication/DisplayDetailsActivity.java
@@ -7,9 +7,14 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.thoughtworks.myapplication.domain.PM25;
+
+import java.io.File;
+
+import retrofit.http.Url;
 
 public class DisplayDetailsActivity extends AppCompatActivity {
 
@@ -40,11 +45,34 @@ public class DisplayDetailsActivity extends AppCompatActivity {
         TextView positionText = (TextView)findViewById(R.id.text_position_name);
         TextView qualityText = (TextView)findViewById(R.id.text_quality);
         TextView timeText = (TextView)findViewById(R.id.text_time);
+        ImageView motionImg = (ImageView)findViewById(R.id.motion_picture);
 
         cityText.setText(pm25.getArea());
         positionText.setText(pm25.getPositionName());
         qualityText.setText(pm25.getQuality());
         timeText.setText(pm25.getTime());
+
+        switch (pm25.getQuality()){
+            case "优":
+                motionImg.setImageResource(R.mipmap.face_one);
+                break;
+            case "良":
+                motionImg.setImageResource(R.mipmap.face_two);
+                break;
+            case "轻度污染":
+                motionImg.setImageResource(R.mipmap.face_three);
+                break;
+            case "中度污染":
+                motionImg.setImageResource(R.mipmap.face_four);
+                break;
+            case "重度污染":
+                motionImg.setImageResource(R.mipmap.face_five);
+                break;
+            case "严重污染":
+                motionImg.setImageResource(R.mipmap.face_six);
+                break;
+        }
+
     }
 
 }
