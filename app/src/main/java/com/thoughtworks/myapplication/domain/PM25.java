cbf3f3e81f8b9352337624785063e558cc464445
crystalynliu <crystalynliu@163.com>
2015-12-25 15:14:37 +0800

    change layout of MainActivity and add the attribute to PM25

--- app/src/main/java/com/thoughtworks/myapplication/domain/PM25.java
+++ app/src/main/java/com/thoughtworks/myapplication/domain/PM25.java
@@ -9,10 +9,21 @@ public class PM25 {
     @SerializedName("quality")
     private String quality;
 
+    @SerializedName("area")
+    private String area;
+
+    @SerializedName("time_point")
+    private String time;
+
+    public String getArea(){ return area ;}
+    public void setArea(String area){ this.area = area; }
+
+    public String getTime(){return time;}
+    public void setTime(String time){this.time = time;}
+    
     public String getPositionName() {
         return positionName;
     }
-
     public void setPositionName(String positionName) {
         this.positionName = positionName;
     }
@@ -20,7 +31,6 @@ public class PM25 {
     public String getQuality() {
         return quality;
     }
-
     public void setQuality(String quality) {
         this.quality = quality;
     }
